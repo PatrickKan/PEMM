9379c95eab5eced2e4b42e269548fdc4ac0e9d5f
gnodipac886 <ericdong886@yahoo.com>
2019-03-02 03:36:12 -0600

    Limit to 1 person

--- Place In Root/json_converter.bat
+++ Place In Root/json_converter.bat
@@ -1,3 +1,3 @@
 @echo off
-bin\OpenPoseDemo.exe --image_dir imagesource/ --write_json output_folder_path/json/ --write_images output_folder_path/rendered_photos/ --write_images_format jpg
+bin\OpenPoseDemo.exe --number_people_max 1 --image_dir imagesource/ --write_json output_folder_path/json/ --write_images output_folder_path/rendered_photos/ --write_images_format jpg
 pause
