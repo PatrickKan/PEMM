a4e5d3fddecbfe7e0c57c70b07a13276087f5de9
gnodipac886 <ericdong886@yahoo.com>
2019-03-02 00:54:39 -0600

    json converter

---
+++ Place In Root/json_converter.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "C:\Users\Eric\Downloads\openpose-1.4.0-win64-cpu-binaries\openpose-1.4.0-win64-cpu-binaries"
+bin\OpenPoseDemo.exe --image_dir imagesource/ --write_json output_folder_path/
+pause
