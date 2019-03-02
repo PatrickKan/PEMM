1d40bc7db07d5db8a0e08f5b8179f157e08e6b40
Patrick Kan <42848321+PatrickKan@users.noreply.github.com>
2019-03-02 03:21:18 -0600

    Rendered Images

--- Place In Root/json_converter.bat
+++ Place In Root/json_converter.bat
@@ -1,3 +1,3 @@
 @echo off
-bin\OpenPoseDemo.exe --image_dir imagesource/ --write_json output_folder_path/
+bin\OpenPoseDemo.exe --image_dir imagesource/ --write_json output_folder_path/json/ --write_images output_folder_path/rendered_photos/ --write_images_format jpg
 pause
