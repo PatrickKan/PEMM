98a2f303ce9f9f5e08f77e3ee1b8747c26ee122e
gnodipac886 <ericdong886@yahoo.com>
2019-03-03 01:59:22 -0600

    fixed messages

--- src/DiffAlgorithm/DecisionMaker.java
+++ src/DiffAlgorithm/DecisionMaker.java
@@ -31,42 +31,42 @@ public class DecisionMaker {
             //deals with joints 5, 6, 7
             if(jointNum == 0)
             {
-                t1 = getFeedback(expected, actual, "holding the ball closer to your chest", "holding the ball away from your chester");
+                t1 = getFeedback(expected, actual, "holding the ball closer to your chest", "holding the ball away from your chest", "left elbow");
             }
             else if(jointNum == 1)//deals with joints 2, 3, 4
             {
-                t2 = getFeedback(expected, actual, "holding the ball closer to the your chest", "holding the ball away from your chest");
+                t2 = getFeedback(expected, actual, "holding the ball closer to your chest", "holding the ball away from your chest", "right elbow");
             }
         } else if(frameNum == 2){
             //Deals with joints 2, 3, 4
             if(jointNum == 0) {
-                t1 = getFeedback(expected, actual,"moving your throwing arm slightly forward", "moving your throwing arm slightly backwards");
+                t1 = getFeedback(expected, actual,"bending your elbow more", "bending your elbow less", "right elbow");
             }
             //deals with joints 12, 13, 14
             else if(jointNum == 1) {
-                t2 = getFeedback(expected, actual, "leaning forward and bending your front leg more", "straightening your front leg less");
+                t2 = getFeedback(expected, actual, "leaning forward and bending your front leg more", "straightening your front leg", "left leg");
             }
         } else if(frameNum == 3){
             //Joints 2, 3, 4
             if(jointNum == 0) {
-                t1 = getFeedback(expected,actual, "moving your throwing arm forward earlier", "moving your throwing arm forward later");
+                t1 = getFeedback(expected,actual, "bending your throwing arm more", "straightening out your throwing arm", "right elbow");
             }
             //Joints 12, 13, 14
             else if(jointNum == 1) {
-                t2 = getFeedback(expected,actual, "leaving your leg a little more bent", "keeping your leg straighter during the throw");
+                t2 = getFeedback(expected,actual, "bending your left leg more during the throw", "straightening your left leg more during the throw", "left leg");
             }
         } else if(frameNum == 4){
             //Joints 2, 3, 4
             if(jointNum == 0) {
-                t1 = getFeedback(expected,actual, "keeping your center mass up with your throwing arm", "using your throwing arm earlier or swing faster");
+                t1 = getFeedback(expected,actual, "bending your throwing arm more", "straightening out your throwing arm", "right elbow");
             }
             //Joints 9, 2, 3
             else if(jointNum == 1) {
-                t2 = getFeedback(expected,actual, "bending your throwing arm slightly", "to maintain your throwing arm moving above your shoulder");
+                t2 = getFeedback(expected,actual, "leaning your body back slightly when you release the ball", "leaning your body forward slightly when you release the ball", "arm and torso");
             }
         } else if(frameNum == 5){
             //Joints 3, 2, 9
-            t1 = getFeedback(expected,actual,"relaxing while throwing. You're using too much power", "putting more thrust into your throw");
+            t1 = getFeedback(expected,actual,"relaxing while throwing. You're using too much power", "putting more thrust into your throw", "arm and torso");
         }
 
         frameCount++;
@@ -77,13 +77,13 @@ public class DecisionMaker {
             String fileName = frameNum + "_decision_";
             if(t1.length() > 0)
             {
-                fw = new FileWriter(new File("src/Data/",fileName + 1));
+                fw = new FileWriter(new File("src/Data/",fileName + 1 + ".txt"));
                 fw.write(t1);
                 fw.close();
             }
             if(t2.length() > 0)
             {
-                fw = new FileWriter(new File("src/Data/", fileName + 2));
+                fw = new FileWriter(new File("src/Data/", fileName + 2 + ".txt"));
                 fw.write(t2);
                 fw.close();
             }
@@ -92,29 +92,47 @@ public class DecisionMaker {
         }
     }
 
-    public static String getFeedback(double expected, double actual, String g, String l){
-        int d = (int)(expected-actual);
-        if (d > 20)
-            return "Not quite there yet, try " + l + ", adjust angle by " + d;
+    public static String getFeedback(double expected, double actual, String g, String l, String part) {
+        int d = (int) (expected - actual);
+        if (!(part.equals("arm and torso")))
+        {
+            if (d > 20)
+                return "Not quite there yet, try " + l + ". \nYour " + part + " angle: " + (int)actual + " degrees \nExpected angle: " + (int)expected + " degrees";
 
-        else if (d < -20)
-            return "Not quite there yet, try " + g + ", adjust angle by " + (-1*d);
+            else if (d < -20)
+                return "Not quite there yet, try " + g + ". \nYour " + part + " angle: " + (int)actual + " degrees \nExpected angle: " + (int)expected + " degrees";
 
-        else if(d > 0) {
-            return "Nice form! Your angle is within the range of (" + (int)(expected - 20.0) + " < " + (int)actual + " < " + (int)(expected + 20) + "). " +
-                    "\nYour form is only " + d +" degrees bigger than professional players. " +
-                    "\nTry " + l + ".";
+            else if (d > 0) {
+                return "Nice form! You could improve by " + l + ".\nYour " + part + " angle: " + (int)actual + " degrees \nExpected angle: " + (int)expected + " degrees";
 
-        }
-        else if(d < 0) {
-            return "Nice form! Your angle is within the range of (" + (int)(expected - 20.0) + " < " + (int)actual + " < " + (int)(expected + 20) + "). " +
-                    "\nYour form is only " + (-1*d) + " degrees smaller than professional players. " +
-                    "\nTry " + g + ".";
+            } else if (d < 0) {
+                return "Nice form! You could improve by " + g + ". \nYour " + part + " angle: " + (int)actual + " degrees \nExpected angle: " + (int)expected + " degrees";
 
-        }else if(d == 0){
-            return "Perfect! Your form here matches 100% with that of professionals!";
+            } else if (d == 0) {
+                return "Great! Your form perfectly matches that of an athlete!";
+            } else {
+                return "Oops, computer vision did not foresee this outcome, you might be from an alternate universe :(.";
+            }
         }
         else
-            return "Oops, computer vision did not foresee this outcome :(.";
+        {
+            if (d > 20)
+                return "Not quite there yet, try " + l + ". \nThe angle between your " + part + ": " + (int)actual + " degrees \nExpected angle: " + (int)expected + " degrees";
+
+            else if (d < -20)
+                return "Not quite there yet, try " + g + ". \nThe angle between your " + part + ": " + (int)actual + " degrees \nExpected angle: " + (int)expected + " degrees";
+
+            else if (d > 0) {
+                return "Nice form! You could improve by " + l + ". \nThe angle between your " + part + ": " + (int)actual + " degrees \nExpected angle: " + (int)expected + " degrees";
+
+            } else if (d < 0) {
+                return "Nice form! You could improve by " + g + ". \nThe angle between your " + part + ": " + (int)actual + " degrees \nExpected angle: " + (int)expected + " degrees";
+
+            } else if (d == 0) {
+                return "Great! Your form perfectly matches that of an athlete!";
+            } else {
+                return "Oops, computer vision did not foresee this outcome, you might be from an alternate universe :(.";
+            }
+        }
     }
 }
